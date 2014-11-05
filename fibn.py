b1b0613a0d3bcab3fc60d4e82a9e16e553a14d96
Michael Krisper <michael.krisper@gmail.com>
2014-11-05 01:28:49 +0100

    argparse

--- fibn.py
+++ fibn.py
@@ -19,12 +19,27 @@ def fib(n):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("num", help="The Fibonacci number you wish to calculate", type=int)
-    args = parser.parse_args()
-    print(args)
 
+    parser.add_argument("num", help="The Fibonacci number you wish to calculate", type=int)
+    parser.add_argument("-o", "--output", help="Output the result to a file", action="store_true")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", action="store_true")
+    group.add_argument("-q", "--quiet", action="store_true")
+
+    args = parser.parse_args()
     result = fib(args.num)
-    print("The {}th fib number is {}".format(args.num, result))
+
+    if args.verbose:
+        print("The {}th fib number is {}".format(args.num, result))
+    elif args.quiet:
+        print(result)
+    else:
+        print("Fib({}) = {}".format(args.num, result))
+
+    if args.output:
+        with open("fibonacci.txt", "a") as f:
+            f.write("{}\n".format(result))
 
 
 if __name__ == "__main__":
